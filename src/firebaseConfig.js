f00e476703e235e6c52f1a7171b2e3a7a87b4551
ConiVC96 <constanzavc.96@outlook.cl>
2022-07-22 11:39:50 -0300

    Instalación de React y Routing

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOHqoWw2J7f1EM0e6EX25PA5PGNyyrE8Y",
+  authDomain: "lab-notes2022.firebaseapp.com",
+  projectId: "lab-notes2022",
+  storageBucket: "lab-notes2022.appspot.com",
+  messagingSenderId: "826897965259",
+  appId: "1:826897965259:web:5fc94a216ce465dfe3bb4e",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
